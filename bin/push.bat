1ea53d6ecbec33d24d7578c3a399ff1e66815608
marsishandsome <marsishandsome@gmail.com>
2016-07-12 13:30:14 +0800

    update

--- bin/push.bat
+++ bin/push.bat
@@ -1,2 +1,5 @@
+FWDIR="$(cd "`dirname "$0"`"/..; pwd)"
+cd $FWDIR
+
 sh.exe --login -i bin/push
 set /p DUMMY=Hit ENTER to continue...
--- slides/bin/gen.bat
+++ slides/bin/gen.bat
@@ -1,4 +1,8 @@
+#!/bin/bash
+
+FWDIR="$(cd "`dirname "$0"`"/..; pwd)"
+cd $FWDIR
+
 landslide SparkMemoryManagement/Index.md -i -d gen/SparkMemoryManagement.html
 landslide HadoopSecurity/Index.md -i -d gen/HadoopSecurity.html
 landslide Spark2.0andSparkAPIHistory/Index.md -i -d gen/Spark2.0andSparkAPIHistory.html
-
