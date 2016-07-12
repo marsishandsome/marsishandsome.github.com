e3a2adc2f60858412e7698807e2984c08ff4f324
marsishandsome <marsishandsome@gmail.com>
2016-07-12 13:26:24 +0800

    update

---
+++ slides/bin/gen.bat
@@ -0,0 +1,4 @@
+landslide SparkMemoryManagement/Index.md -i -d gen/SparkMemoryManagement.html
+landslide HadoopSecurity/Index.md -i -d gen/HadoopSecurity.html
+landslide Spark2.0andSparkAPIHistory/Index.md -i -d gen/Spark2.0andSparkAPIHistory.html
+
--- slides/gen.bat
+++
@@ -1,4 +0,0 @@
-landslide SparkMemoryManagement/Index.md -i -d SparkMemoryManagement.html
-landslide HadoopSecurity/Index.md -i -d HadoopSecurity.html
-landslide Spark2.0andSparkAPIHistory/Index.md -i -d Spark2.0andSparkAPIHistory.html
-
